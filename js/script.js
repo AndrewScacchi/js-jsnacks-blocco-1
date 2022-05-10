28662215483b0c9a8fd9435abaa02ab9f98fae13
andrea scacchi <andrea.scacchi@live.com>
2022-05-10 18:14:10 +0200

    final-evening commit. still blocked. can create ul inside li but no matter what i write can't create another element inside this one.

--- js/script.js
+++ js/script.js
@@ -14,37 +14,63 @@ const arraySmall = [
     "cotto alla piastra"
 ];
 
-console.log(arrayBig);
-console.log(arraySmall);
+// console.log(arrayBig);
+// console.log(arraySmall);
 
 const ulExt = document.querySelector("ul.list-external");
 
-// creo una UL con LI in base all'arrey dato
+// creo una UL con LI in base all'array dato
 for (i = 0; i < arrayBig.length ; i++){
     const liExt = document.createElement(`li`);
+    const ulInt = document.createElement("ul");
 
     liExt.classList.add("li-ext");
-    liExt.classList.add(`li-ext-${i+1}`);
+    // liExt.classList.add(`li-ext-${i+1}`);
     //appending for LI to UL and later content to LI
     ulExt.append(liExt);
     liExt.append(arrayBig[i]);
-
-    let liInt = document.querySelector(`li-ext-${i+1}`);
-    console.log(liInt);
-
-    
-   
-}
-//for each new LI created, create a new UL and an LI inside
-for (i = 0; i < arrayBig.length ; i++){
-    const liExt = document.querySelector(`.li-ext-${i+1}`);
-    const ulInt = document.createElement("ul");
-    
     liExt.append(ulInt);
-    ulInt.classList.add(`ul-int-${i+1}`);
+    ulInt.classList.add(`.ul-int-${i+1}`);
+    for (e = 0; e < arraySmall.length; e++){
+        const ulCreated = document.querySelector(`.ul-int-${i+1}`);
+        const liInt = document.createElement("li");
+        
+        console.log(arraySmall[e]);
+        //il dannato console log funziona.
+
+
+
+        //PERCHE' CAZZO NON FUNZIONI STRONZO!
+        // ulCreated.append(liInt);
+        // liInt.append(arraySmall[e]);
+    }
+
+    // for (i=0; i < arraySmall.length ; i++){
+    //     const ulCreated = document.querySelector(`.ul-int-${i+1}`);
+        
+    //     const liInt = document.createElement("li");
+
+    //     ulCreated.append(liInt);
+    //     liInt.append(arraySmall[i]);
+    // }
+    // 
+
+    // let liInt = document.querySelector(`li-ext-${i+1}`);
+    // console.log(liInt);
+
+    
+    
 }
 
-//ora per ogni ul-int-nn devo creare una nuova serie di LI
+//for each new LI created, create a new UL and an LI inside
+// for (i = 0; i < arrayBig.length ; i++){
+//     const liExt = document.querySelector(`.li-ext-${i+1}`);
+//     const ulInt = document.createElement("ul");
+    
+//     liExt.append(ulInt);
+//     ulInt.classList.add(`ul-int-${i+1}`);
+// }
+//ora per ogni ul-int-nn devo creare una nuova serie di LI devo selezionare ogni singolo UL interno in base alla suo classe..e per ognuno di questi creare un ulteriore ciclo di LI.. è un ciclo dentro ad un altro ciclo... come ovviare?
 
 // for (i = 0; i < arraySmall.length ; i++){
 //     const ulInt = document.querySelector(`.ul-int-${i+1}`);
