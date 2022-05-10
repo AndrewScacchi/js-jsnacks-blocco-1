dff7d2e566c5c06acc6a3759e67f614cd9856fb3
andrea scacchi <andrea.scacchi@live.com>
2022-05-10 16:27:31 +0200

    mid-initial commit, created first li for UL, now onto sub UL..

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 // JAVASCRIPT for the EXERCISE
+console.log("Hello World!");
+// Creare due array, 5 stringhe il primo, 2 il secondo
+
+const arrayBig = [
+    "coccodrillo",
+    "gatto",
+    "cane",
+    "piccione",
+    "porceddu"
+];
+const arraySmall = [
+    "cotto alla griglia",
+    "cotto alla piastra"
+];
+
+console.log(arrayBig);
+console.log(arraySmall);
+
+const arrayExt = document.querySelector("ul.list-external");
+
+for (i = 0; i < arrayBig.length ; i++){
+    arrayExt.innerHTML += `<li class="li-ext"> ${arrayBig[i]} </li>`;
+    
+
+}
 
